31fa9fef864e64de5abd38b80e6de2793cfbd8f4
ToriSmash <Victoria.Mckenzie1@mds.ac.nz>
2023-02-26 15:16:47 +1300

    Level Hub Setup

--- Assets/MainMenu/Scripts/Portal.cs
+++ Assets/MainMenu/Scripts/Portal.cs
@@ -1,31 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
-    public int LevelNum;
-    public bool StarterDoor;
-    bool LockedDoor = false;
+    //public static int levelsExplored; // Amount of rooms explored in a run 
+    //const int maxLevelsExplored = 15; // The amount of rooms a player can explore until endgame *pressuming narrative based game
+    //private int minRandomLevel = 2;   // Minimum level value 
+    //private int maxRandomLevel = 5;   // Maximum level value
+    public int LevelNum;              // Value of the room which a door leads through
+    public bool starterDoor;
+    bool lockedDoor = false;
+
+    public Animator transition;
+    public float transitionTime = 2.0f;
+
+    IEnumerator LoadLevel(GameObject other)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        //transition.SetTrigger("EndWait");
+        //other.gameObject.GetComponent<CharacterController>().enabled = false;
+        //other.transform.position = GameObject.Find("Starting Spot " + LevelNum).transform.position;
+        //other.gameObject.GetComponent<CharacterController>().enabled = true;
+        SceneManager.LoadScene(LevelNum + 2);
+    }
 
     private void LockChangeDoor()
     {
-        LockedDoor = !LockedDoor;
+        lockedDoor = !lockedDoor;
     }
 
     private void Start()
     {
-        if (!StarterDoor)
-        {
-            LevelNum = Random.Range(0, 3);
-        }
+        //if (!starterDoor && levelsExplored % 5 < 3)
+        //{
+        ////    LevelNum = Random.Range(minRandomLevel, maxRandomLevel);
+        //}
+        //else if (!starterDoor)
+        //{
+        ////    LevelNum = 6;
+        //}
+        //transition = GameObject.Find("Image").GetComponent<Animator>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            print(LevelNum);
+            //Teleport in the same level
+            //print("Starting Spot " + LevelNum);
+            //other.gameObject.GetComponent<CharacterController>().enabled = false;
+            //other.transform.position = GameObject.Find("Starting Spot " + LevelNum).transform.position;
+            //other.gameObject.GetComponent<CharacterController>().enabled = true;
+
+            //print(LevelsExplored);
+            //LevelsExplored++;
+            //print(LevelsExplored);
+
+            StartCoroutine(LoadLevel(other.gameObject));
         }
     }
 }
