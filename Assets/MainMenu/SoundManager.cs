2a7d5331edd7b2bd9046c02d4315de6bc0ee28b7
ToriSmash <Victoria.Mckenzie1@mds.ac.nz>
2023-03-04 16:41:47 +1300

    Renaming sound manager + trying to figure out how shaders work in HDRP

--- Assets/MainMenu/SoundManager.cs
+++ Assets/MainMenu/SoundManager.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
+public static class SoundManager
 {
     public static int musicVolume;
     public static int soundVolume;
 
-
+    public static void PlaySound() { }
 
 }
