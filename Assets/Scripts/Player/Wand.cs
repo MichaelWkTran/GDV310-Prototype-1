a49650d3c740e77ef5bf15bb460f8cc0bee0700e
Sean <88115321+XsDpX16@users.noreply.github.com>
2023-03-06 14:11:28 +1300

    Player can now dash in the direction theyre moving
    Also added some smaller things

--- Assets/Scripts/Player/Wand.cs
+++ Assets/Scripts/Player/Wand.cs
@@ -53,7 +53,7 @@ public class Wand : MonoBehaviour
 
     void BasicWandAttack()
     {
-        GameObject copy = (GameObject)Instantiate(projectile, wandEnd.transform.position + (wandEnd.transform.forward * 1.001f), Quaternion.identity); // copy of basic attack object
+        GameObject copy = (GameObject)Instantiate(projectile, wandEnd.transform.position + (wandEnd.transform.forward * 1.01f), Quaternion.identity); // copy of basic attack object
         Rigidbody rb = copy.AddComponent(typeof(Rigidbody)) as Rigidbody;                                                                              // add rigid body to new object
         rb.freezeRotation = true;                                                                                                                      // no rotation
         rb.useGravity = false;                                                                                                                         // no gravity
