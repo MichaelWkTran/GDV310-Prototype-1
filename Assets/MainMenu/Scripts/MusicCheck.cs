bd34cbe38add9a2332e72137f480e992b9e82466
ToriSmash <Victoria.Mckenzie1@mds.ac.nz>
2023-03-10 14:52:26 +1300

    Music Should Play based on if enemies are present

--- Assets/MainMenu/Scripts/MusicCheck.cs
+++ Assets/MainMenu/Scripts/MusicCheck.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class MusicCheck : MonoBehaviour
 {
     public bool musicShouldPlay = true;
+    public static int enemyCount;
     private void Start()
     {
         SoundManager.InitialiseMusicPlayer();
         SoundManager.MusicVolumeChange(SoundManager.musicVolume);
         SoundManager.SoundEffectVolumeChange(SoundManager.soundVolume);
         SoundManager.EnemyVolumeChange(SoundManager.enemyVolume);
+        Enemy[] Array = FindObjectsOfType<Enemy>();
+        enemyCount = Array.Length;
     }
 
     private void Update()
@@ -19,7 +22,14 @@ public class MusicCheck : MonoBehaviour
         //enemies that would detect the player and this would trigger different music for the player
         if (!SoundManager.musicPlayer.GetComponent<AudioSource>().isPlaying && musicShouldPlay)
         {
-            SoundManager.PlayMusic(SoundManager.Music.NonBattle);
+            if (enemyCount > 0)
+            {
+                SoundManager.PlayMusic(SoundManager.Music.Battle);
+            }
+            else
+            {
+                SoundManager.PlayMusic(SoundManager.Music.NonBattle);
+            }
         }
     }
 }
